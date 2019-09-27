54e3d8f607a8eedcc0e36f1fb3b7e3a73d7ff67d
moira <moira.taylor.2023@student.lhschools.org>
2019-09-27 09:31:20 -0400

    declared variables

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 public class Magic8Ball {
 
-    public static void main(String[]args){
-        
+    public static void main(String[] args) {
+
+        //need 5 positive and 5 negative - 10 different choices
+        //use the input from user & # that they give between 1-100
+        //0-10(outlook does not look good) 11-20(there is a bright future ahead)
+        //21-30(not possible) 31-40(As I see it, yes)
+        // 41-50(don't count on it) 51-60(it is certain)
+        // 61-70(very doubtful) 71-80(you may rely on it)
+        // 81-90(my sources say no) 91-100(without a doubt)
+
+
+        Scanner Keyboard;
+        String question;
+        float number;
+
+        Keyboard = new Scanner(System.in);
 
 
 
 
 
-    }
 
-}
+
